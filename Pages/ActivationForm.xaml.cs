28b7e8f3ad0af12dd2c727d130755c925d4b8617
Xavier Julies <78542365+XavierJ96@users.noreply.github.com>
2024-08-18 11:59:22 +0200

    feat: add Activation Form

--- Pages/ActivationForm.xaml.cs
+++ Pages/ActivationForm.xaml.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using System;
 using System.Windows;
 using System.Windows.Controls;
+using CryplexAdmin.Models;
+using System.Collections.Generic;
 
 namespace CryplexAdmin.Pages
 {
@@ -12,25 +14,98 @@ namespace CryplexAdmin.Pages
     /// </summary>
     public partial class ActivationForm : Page
     {
+
+        private readonly ProductService _productService;
+        private readonly LicenseService _licenseService;
+        private readonly LicenseTemplateService _licenseTemplateService;
+
         public ActivationForm()
         {
             InitializeComponent();
+            _productService = new ProductService(new HttpClientService());
+            _licenseService = new LicenseService(new HttpClientService());
+            _licenseTemplateService = new LicenseTemplateService(new HttpClientService());
+            _ = GetLicenceInfoAsync();
+        }
+
+        public async Task GetLicenceInfoAsync()
+        {
+            try
+            {
+                List<License> licenses = await _licenseService.GetAllLicensesAsync();
+                if (licenses != null)
+                {
+                    licenseComboBox.ItemsSource = licenses;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while fetching products: {ex.Message}");
+            }
+        }
+
+        private async void licenseComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (licenseComboBox.SelectedItem is License selectedLicense)
+            {
+                var productData = await _productService.GetProductDataAsync(selectedLicense.ProductId);
+
+                productIdBox.Text = selectedLicense.ProductId;
+                productDataBox.Text = productData;
+
+                statusLabel.Text = "Status: License selected";
+
+                try
+                {
+                    LexActivator.SetProductData(productData);
+                    LexActivator.SetProductId(selectedLicense.ProductId, LexActivator.PermissionFlags.LA_USER);
+
+                    int status = LexActivator.IsLicenseGenuine();
+                    if (status == LexStatusCodes.LA_OK || status == LexStatusCodes.LA_EXPIRED || status == LexStatusCodes.LA_SUSPENDED || status == LexStatusCodes.LA_GRACE_PERIOD_OVER)
+                    {
+                        uint expiryDate = LexActivator.GetLicenseExpiryDate();
+                        int daysLeft = (int)(expiryDate - unixTimestamp()) / 86400;
+                        statusLabel.Text = "License genuinely activated! Activation Status: " + status.ToString();
+                        activateBtn.Content = "Deactivate";
+                        activateTrialBtn.IsEnabled = false;
+
+                        return;
+                    }
+                    status = LexActivator.IsTrialGenuine();
+                    if (status == LexStatusCodes.LA_OK)
+                    {
+                        uint trialExpiryDate = LexActivator.GetTrialExpiryDate();
+                        int daysLeft = (int)(trialExpiryDate - unixTimestamp()) / 86400;
+                        statusLabel.Text = "Trial period! Days left:" + daysLeft.ToString();
+                        activateTrialBtn.IsEnabled = false;
+                    }
+                    else if (status == LexStatusCodes.LA_TRIAL_EXPIRED)
+                    {
+                        statusLabel.Text = "Trial has expired!";
+                    }
+                    else
+                    {
+                        statusLabel.Text = "Trial has not started or has been tampered: " + status.ToString();
+                    }
+                }
+                catch (LexActivatorException ex)
+                {
+                    statusLabel.Text = "Error code: " + ex.Code.ToString() + " Error message: " + ex.Message;
+                }
+            }
         }
 
         private void activateBtn_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                if (string.IsNullOrWhiteSpace(productKeyBox.Text) ||
+                if (string.IsNullOrWhiteSpace(licenseComboBox.Text) ||
                     string.IsNullOrWhiteSpace(productIdBox.Text) ||
                     string.IsNullOrWhiteSpace(productDataBox.Text))
                 {
                     statusLabel.Text = "Please fill in all fields before activating.";
                     return;
                 }
-
-                LexActivator.SetProductData(productDataBox.Text);
-                LexActivator.SetProductId(productIdBox.Text, LexActivator.PermissionFlags.LA_USER);
 
                 if (activateBtn.Content.ToString() == "Deactivate")
                 {
@@ -39,6 +114,7 @@ namespace CryplexAdmin.Pages
                     {
                         statusLabel.Text = "License deactivated successfully";
                         activateBtn.Content = "Activate";
+                        ClearInputs();
                         activateTrialBtn.IsEnabled = true;
                     }
                     else
@@ -48,7 +124,7 @@ namespace CryplexAdmin.Pages
                 }
                 else
                 {
-                    LexActivator.SetLicenseKey(productKeyBox.Text);
+                    LexActivator.SetLicenseKey(licenseComboBox.Text);
                     int status = LexActivator.ActivateLicense();
                     if (status == LexStatusCodes.LA_OK || status == LexStatusCodes.LA_EXPIRED || status == LexStatusCodes.LA_SUSPENDED)
                     {
@@ -72,19 +148,22 @@ namespace CryplexAdmin.Pages
 
         private void activateTrialBtn_Click(object sender, RoutedEventArgs e)
         {
+
+            LexActivator.SetProductData(productDataBox.Text);
+            LexActivator.SetProductId(productIdBox.Text, LexActivator.PermissionFlags.LA_USER);
+
             try
             {
                 LexActivator.SetTrialActivationMetadata("key", "value");
                 int status = LexActivator.ActivateTrial();
-                if (status == LexStatusCodes.LA_OK)
+                if (status != LexStatusCodes.LA_OK)
                 {
-                    statusLabel.Text = "Trial activated successfully!";
-
+                    statusLabel.Text = "Error activating the trial";
                     ClearInputs();
                 }
                 else
                 {
-                    statusLabel.Text = "Error activating the trial";
+                    statusLabel.Text = "Trial activated successfully!";
                 }
             }
             catch (LexActivatorException ex)
@@ -95,9 +174,16 @@ namespace CryplexAdmin.Pages
 
         private void ClearInputs()
         {
-            productKeyBox.Text = string.Empty;
+            licenseComboBox.Text = string.Empty;
             productIdBox.Text = string.Empty;
             productDataBox.Text = string.Empty;
+            activateBtn.Content = "Activate";
+            activateTrialBtn.Content = "Activate Trial";
+        }
+
+        private uint unixTimestamp()
+        {
+            return (uint)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
         }
 
         private void productKeyBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
