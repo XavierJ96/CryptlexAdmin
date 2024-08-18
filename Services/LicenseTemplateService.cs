0047f1ec5b94920d54053b56351cc4969d08aa00
Xavier Julies <78542365+XavierJ96@users.noreply.github.com>
2024-08-18 11:52:08 +0200

    feat: add GetAllLicenseTemplates

---
+++ Services/LicenseTemplateService.cs
@@ -0,0 +1,41 @@
+﻿using CryplexAdmin.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryplexAdmin.Services
+{
+    internal class LicenseTemplateService
+    {
+
+        private readonly HttpClientService _httpClientService;
+
+        public LicenseTemplateService(HttpClientService httpClientService)
+        {
+            _httpClientService = httpClientService;
+        }
+
+        public async Task<List<LicenseTemplate>> GetAllLicenseTemplatesAsync()
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClientService.GetAsync("products");
+                response.EnsureSuccessStatusCode();
+                string content = await response.Content.ReadAsStringAsync();
+
+                var productResponseList = JsonConvert.DeserializeObject<List<LicenseTemplate>>(content);
+
+                return productResponseList;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
